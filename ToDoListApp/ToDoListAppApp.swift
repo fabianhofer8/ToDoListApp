f48e4369c2444555d6020fa0074ce26b917027a4
hofabi <s2010237027@fhooe.at>
2024-05-31 14:25:46 +0200

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Fabian Hofer on 31.05.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
